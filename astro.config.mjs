25dd1460e9f8fd9213baec51b0f608040f27a56c
mletterio <michaelletterio@gmail.com>
2024-09-15 12:42:14 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mletterio.github.io',
-	base: '/mletterio_site',
 	integrations: [mdx(), sitemap()],
 });
